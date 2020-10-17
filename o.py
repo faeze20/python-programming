c28ead4677b02949c6f11da914423a2fe3fb1203
faeze20 <65281313+faeze20@users.noreply.github.com>
2020-10-17 22:19:27 +0330

    ayyoub.akbari@gmail.com

--- o.py
+++ o.py
@@ -1,1 +1,13 @@
-
+file1=open("C:\\Users\\Hamrah System\\Desktop\\m.txt","r+")
+file1.writelines(input("enter strings"))
+file1=open("C:\\Users\\Hamrah System\\Desktop\\m.txt","a+")
+file1.readline()
+b=input("entar char")
+c=0
+for i in file1:
+    if i==b:
+        c+=1
+        file1.readline() 
+print("repeat is",c)        
+file1.close()
+      
